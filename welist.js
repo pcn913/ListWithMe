6860185d201dfb9f9f3c4ed7560dc30cf8ebec59
Prasanth Nannapaneni <prasanth.nannapaneni@northwestern.edu>
2017-05-22 18:56:12 -0500

    -latest and greatest changes for welist

--- welist.js
+++ welist.js
@@ -10,7 +10,7 @@ var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -22,16 +22,16 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
-app.use(Expressess.static("./public"));
+app.use(express.static("./public"));
 
 // Routes =============================================================
 
-// require("./routes/html-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
+ require("./routes/html-routes.js")(app);
+ require("./routes/list-api-routes.js")(app);
 // require("./routes/author-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
